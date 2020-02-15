69273ab720e86d5b58b7af4fc5df40d99518eb9c
Michael Keller <mkeller2013@fau.edu>
2020-02-15 00:20:11 -0500

    Implement ensemble 10-fold cross-validation

--- Scripts/Diff_CNN.py
+++ Scripts/Diff_CNN.py
@@ -3,8 +3,10 @@ import pandas as pd
 import numpy as np
 import cv2
 import tensorflow as tf
-from sklearn.model_selection import KFold
-from sklearn.metrics import confusion_matrix
+import math
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split, KFold
+from sklearn.metrics import accuracy_score
 
 os.chdir('/home/mkeller2013/MTS_NN_Project')
 
@@ -49,12 +51,15 @@ def load_data(n_samples):
             data[count] = img        
             count=count+1
             print("Loaded file "+str(count)+ " of "+str(15*n_samples)+ " ")
-    return data              
+    
+    data=data.astype('float32')    
+    data /= 255
+
+    return data             
 
 def preprocess_data(data):
     temp = np.empty((15, 115, 110))
     diff_data = np.empty((n_samples+1, 14, 115, 110))
-
     count = 0
 
     for i, frame in enumerate(data):
@@ -68,6 +73,7 @@ def preprocess_data(data):
 
     diff_data = diff_data[0:n_samples]
     diff_data = np.moveaxis(diff_data, 1, 3)
+    
     return diff_data
 
 def create_labels():    
@@ -82,10 +88,6 @@ def create_labels():
         diff_labels[i] = temp
     return diff_labels
 
-def split_data(data):
-    train_data, test_data, train_labels, test_labels=train_test_split(diff_data, diff_labels, test_size=0.3, train_size=0.7, shuffle=True)
-    return train_data, test_data, train_labels, test_labels
-
 def set_memory_growth():
     gpus = tf.config.experimental.list_physical_devices('GPU')
     if gpus:
@@ -99,7 +101,7 @@ def set_memory_growth():
             # Memory growth must be set before GPUs have been initialized
             print(e)
 
-def create_builtin_model():
+def create_single_model(batch_size, train_data, val_data, train_labels, val_labels):
     #Xception=tf.keras.applications.xception.Xception(include_top=False, weights=None, input_tensor=None, input_shape=(115, 110, 14), pooling=None, classes=2) 
     VGG16=tf.keras.applications.vgg16.VGG16(include_top=False, weights=None, input_tensor=None, input_shape=(115, 110, 14), pooling=None, classes=2)
     #VGG19=tf.keras.applications.vgg19.VGG19(include_top=False, weights=None, input_tensor=None, input_shape=(115, 110, 14), pooling=None, classes=2)
@@ -120,31 +122,38 @@ def create_builtin_model():
     x = base_model.output
     x = tf.keras.layers.GlobalAveragePooling2D()(x)    
     predictions = tf.keras.layers.Dense(2, activation= 'softmax')(x)
-    adam = tf.keras.optimizers.Adam(lr=0.00001)
-
+    adam = tf.keras.optimizers.Adam(lr=math.sqrt(batch_size)*(10**-5))
     model = tf.keras.models.Model(inputs = base_model.input, outputs = predictions)
+    mc=tf.keras.callbacks.ModelCheckpoint('Models/best_model-VGG16.hdf5', save_best_only=True, monitor='val_accuracy')    
     model.compile(optimizer=adam, loss='binary_crossentropy', metrics=['accuracy'])
-    return model
+    model.save_weights('Models/initial_weights.h5')
 
-def run_experiment(model, runs, folds, train_data, train_labels, test_data, test_labels):
-    for i in range(0, runs):
-        print("Run ", i+1)
-        count=1    
+    model.fit(train_data, train_labels, epochs=30, batch_size=batch_size, validation_data=(val_data, val_labels), shuffle=True, callbacks=[mc])
 
-        for train_index, val_index in KFold(folds, shuffle=True).split(train_data):
-            train_data, val_data=train_data[train_index], train_data[val_index]
-            train_labels, val_labels=train_labels[train_index], val_labels[val_index]
+    model=tf.keras.models.load_model('Models/best_model-VGG16.hdf5')
+    # evaluate the model
+    test_loss, test_acc = model.evaluate(val_data, val_labels, batch_size=batch_size, verbose=0)
+    return model, test_acc
 
-            model.save_weights("weights.h5")
-
-            print("Fold ", count)
-                                                       
-            hist=model.fit(train_data, train_labels, epochs=50, batch_size=16, validation_data = (val_data, val_labels), verbose=1, shuffle=True)
-            test_results=model.evaluate(test_data, test_labels, batch_size=16)
-
-            print(test_results)
-
-            model.load_weights("weights.h5")
+def ensemble_predictions(members, test_data):
+	# make predictions
+	predictions = [model.predict(test_data) for model in members]
+	predictions = np.array(predictions)
+	# sum across ensemble members
+	summed = np.sum(predictions, axis=0)
+	# argmax across classes
+	result = np.argmax(summed, axis=1)
+	return result
+ 
+# evaluate a specific number of members in an ensemble
+def evaluate_n_members(members, n_members, test_data, test_labels):
+    # select a subset of members
+    subset = members[:n_members]
+    # make prediction
+    en_predictions = ensemble_predictions(subset, test_data)
+    en_predictions = tf.keras.utils.to_categorical(en_predictions)
+    # calculate accuracy
+    return accuracy_score(test_labels, en_predictions)
 
 n_balls = len(os.listdir('Data/Balls/Balls'))
 n_strikes = len(os.listdir('Data/Strikes/Strikes'))
@@ -153,8 +162,36 @@ n_samples = n_balls + n_strikes
 data=load_data(n_samples)
 diff_data=preprocess_data(data)
 labels=create_labels()
-train_data, test_data, train_labels, test_labels=split_data(diff_data)
 set_memory_growth()
-model=create_builtin_model()
-run_experiment(model, 20, 10, train_data, train_labels, test_data, test_labels)
+train_data, test_data, train_labels, test_labels=train_test_split(diff_data, labels, test_size=0.3, shuffle=True)
 
+n_folds = 10
+kfold = KFold(n_folds, True, 1)
+# cross validation estimation of performance
+scores, members = list(), list()
+
+for train_index, test_index in kfold.split(train_data):
+	# select samples
+	train_data_cv, train_labels_cv = train_data[train_index], train_labels[train_index]
+	val_data, val_labels = train_data[test_index], train_labels[test_index]
+	# evaluate model
+	model, test_acc = create_single_model(4, train_data_cv, val_data, train_labels_cv, val_labels)
+	print('>%.3f' % test_acc)
+	scores.append(test_acc)
+	members.append(model)
+# summarize expected performance
+print('Estimated Accuracy %.3f (%.3f)' % (np.mean(scores), np.std(scores)))
+# evaluate different numbers of ensembles on hold out set
+single_scores, ensemble_scores = list(), list()
+for i in range(1, n_folds+1):
+	ensemble_score = evaluate_n_members(members, i, test_data, test_labels)
+	_, single_score = members[i-1].evaluate(test_data, test_labels, verbose=0)
+	print('> %d: single=%.3f, ensemble=%.3f' % (i, single_score, ensemble_score))
+	ensemble_scores.append(ensemble_score)
+	single_scores.append(single_score)
+# plot score vs number of ensemble members
+print('Accuracy %.3f (%.3f)' % (np.mean(single_scores), np.std(single_scores)))
+x_axis = [i for i in range(1, n_folds+1)]
+plt.plot(x_axis, single_scores, marker='o', linestyle='None')
+plt.plot(x_axis, ensemble_scores, marker='o')
+plt.show()
